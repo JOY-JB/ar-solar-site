1cedd5f324fb4f3853ef3bc6f55478b3d8a77b12
Rohan1279 <muniruddinrohan@gmail.com>
2024-02-26 01:31:00 +0600

    add carousal in testimonials section

--- src/app/components/Testimonial/Testimonial.jsx
+++ src/app/components/Testimonial/Testimonial.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import person1 from "../../../../public/assets/images/testimonial/testimonial1.png";
 import Image from "next/image";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 const Testimonial = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#1B2025] from-20% to-[#08090B]">
@@ -25,6 +34,23 @@ const Testimonial = () => {
       <div>
         <Image src={person1} alt="person-1" />
       </div> */}
+        <Carousel className="w-full max-w-xs mx-auto">
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <Image src={person1} alt="person-1" className="w-full" />
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
