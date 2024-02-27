19c69fedf1b55bc22d0f4e24d06bd0825b90b98e
Rohan1279 <muniruddinrohan@gmail.com>
2024-02-27 22:25:51 +0600

    add quote route

---
+++ src/app/quote/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#1B2025] from-20% to-[#08090B] text-white py-[92px]"></div>
+  );
+};
+
+export default page;
